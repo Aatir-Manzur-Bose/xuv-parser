8b6b0b80286bf84ed9c1b6abb8e70ad3b82a5125
Tyree-Doss-Bose <tyree_doss@bose.com>
2023-05-31 12:11:10 -0400

    tyree's changes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # This is a sample Python script.
 
+# look out for where the partition starts from, partitions are shown from lines 36-45
+# offset is length of preceding partitions combined
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -31,6 +34,8 @@ flash_schema = {
         ("ro_fs", {"capacity": 1057 * 4096, "authenticate": False})
     ]
 }
+
+# divide by 4 to represent sectors within partitions
 curator_fs_array = bytearray([0xde, 0xad, 0xbe, 0xef] * ((3 * 4096) // 4))
 apps_p0_array = bytearray([0xde, 0xad, 0xbe, 0xef] * ((162 * 4096) // 4))
 apps_p1_array = bytearray([0xde, 0xad, 0xbe, 0xef] * ((554 * 4096) // 4))
@@ -42,9 +47,35 @@ ra_partition_array = bytearray([0xde, 0xad, 0xbe, 0xef] * ((69 * 4096) // 4))
 ro_cfg_fs_array = bytearray([0xde, 0xad, 0xbe, 0xef] * ((53 * 4096) // 4))
 ro_fs_array = bytearray([0xde, 0xad, 0xbe, 0xef] * ((1057 * 4096) // 4))
 
-sections = {"curator_fs": {}, "apps_p0": {}}
+sections = {"curator_fs": {}, "apps_p0": {}, "apps_p1": {}, "device_ro_fs": {},
+            "rw_config": {}, "rw_fs": {}, "debug_partition": {}, "ra_partition": {}
+            , "ro_cfg_fs": {}, "ro_fs": {}}
+names = ["curator_fs", "apps_p0", "apps_p1", "device_ro_fs", "rw_config", "rw_fs", "debug_partition", "ra_partition",
+         "ro_cfg_fs", "ro_fs"]
+capacities = [(3 * 4096)//4, (162 * 4096)//4, (554 * 4096)//4, (16 * 4096)//4, (32 * 4096)//4, (1 * 4096)//4, (99 * 4096)//4, (69 * 4096)//4,
+              (53 * 4096)//4, (1057 * 4096)//4]
+
+partition_capacities = [[curator_fs_array, 0, 0], [apps_p0_array, 0, 0], [apps_p1_array, 0, 0], [device_ro_fs_array, 0, 0],
+                        [rw_config_array, 0, 0], [rw_fs_array, 0, 0], [debug_partition_array, 0, 0],
+                        [ra_partition_array, 0, 0], [ro_cfg_fs_array, 0, 0], [ro_fs_array, 0, 0]]
+
+file_ranges_max = [None] * 10
+file_ranges_max[0] = (3 * 4096) // 4
+for x in range(1, len(capacities)):
+    file_ranges_max[x] = capacities[x] + file_ranges_max[x - 1]
+
+file_ranges_min = [None] * 10
+file_ranges_min[0] = 0
+for y in range(1, len(file_ranges_max)):
+    file_ranges_min[y] = file_ranges_max[y-1]
+
+for j in range(len(partition_capacities)):
+    partition_capacities[j][2] = file_ranges_max[j]
+for k in range(len(partition_capacities)):
+    partition_capacities[j][1] = file_ranges_min[j]
 
 
+# modify i (offset) as well as while loop variables
 def read_section_from_xuv():
     f = open("flash_image.xuv", "r")
     g = open("flash_image_formatted.xuv", "w")
@@ -57,19 +88,33 @@ def read_section_from_xuv():
     g.close()
     with open('flash_image_formatted.xuv', 'rb') as f:
         hexdata = f.read().hex()
+
+    # i = 0
+    # while i < capacity:
+    #     partition[i] = int(hexdata[i], 16)
+    #     i += 1
+
     i = 0
-    while i < (3 * 4096):
-        curator_fs_array[i] = int(hexdata[i], 16)
-        i += 1
+    for j in range(len(partition_capacities)):
+        k = 0
+        while partition_capacities[j][1] <= i < partition_capacities[j][2]:
+            partition_capacities[j][0][k] = int(hexdata[i], 16)
+            i += 1
+            k += 1
 
 
+
+# modify i (offset) and while loop variables... j = 3 * 4096 ... while j < 162 * 4096 (second partition size)
+# run find length for each section array
 def find_length() -> int:
-    i = 0
-    sections["curator_fs"]["size"] = 888
-    while i < 3 * 4096:
-        if curator_fs_array[i] == 0xde:
-            sections["curator_fs"]["size"] = i
-        i += 1
+    for z in range(len(capacities)):
+        i = 0
+        while i < capacities[z]:
+            if hex(partition_capacities[z][0][i]) == '0xd':
+                print("here")
+                sections.get(names[z])["size"] = i
+                break
+            i += 1
 
 
 def print_flash_usage_table():
@@ -100,9 +145,12 @@ def print_flash_usage_table():
     capacity_blocks = (3 * 4096) // block_size
     capacity = "{:2d}".format(capacity_blocks)
 
+    # dictionary within a dictionary
     for section in sections:
         name = section
         print(name)
+        # This line starts another nested loop. It iterates over the key-value pairs within the dictionary associated
+        # with the current. j is the key and k is the value (size (j) = k)
         for j, k in sections[section].items():
             used_blocks = k // block_size + (k % block_size > 0)
             used = "{:2d}".format(used_blocks)
@@ -114,10 +162,10 @@ def print_flash_usage_table():
 
             table_data.append(data_row.format(name, capacity, used, unused, size, w=column_width))
 
-            totals['capacity'] += capacity_blocks
-            totals['used'] += used_blocks
-            totals['unused'] += unused_blocks
-            totals['size'] += k
+            totals['capacity'] = int(totals['capacity']) + capacity_blocks
+            totals['used'] = int(totals['used']) + used_blocks
+            totals['unused'] = int(unused_blocks)
+            totals['size'] = int(k)
 
             totals['capacity'] = "{:2d}".format(totals['capacity'])
             totals['used'] = "{:2d}".format(totals['used'])
@@ -139,9 +187,8 @@ def print_stats():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    find_length()
     read_section_from_xuv()
-
+    find_length()
     print_flash_usage_table()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
